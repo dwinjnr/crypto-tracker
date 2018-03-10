c60688567b98121649310beab607b1deb7122dfc
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-03-10 08:35:51 +0100

    switch

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -50,6 +50,21 @@
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
+      <v-list>
+      <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>person</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <v-container fluid>
+                <v-switch :label="`${switch1.toString()}`" v-model="login"></v-switch>
+                <v-switch :label="`${switch2.toString()}`" v-model="login"></v-switch>
+              </v-container>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar app fixed clipped-left>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
